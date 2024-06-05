4a18176bc15c568bb98c89fd209c55e6c5be176f
Benjamin Huh <benjaminayhuh@gmail.com>
2024-06-05 12:29:15 -0700

    attempted excercise tasks

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿namespace SntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
         }
     }
 }
